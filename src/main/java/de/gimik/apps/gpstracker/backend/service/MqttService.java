c275d2564fb0fa64763e3c276a805e101a18cd75
window <hoangg1139@gmail.com>
2026-06-26 16:46:58 +0700

    some fix

--- src/main/java/de/gimik/apps/gpstracker/backend/service/MqttService.java
+++
@@ -1,7 +0,0 @@
-package de.gimik.apps.gpstracker.backend.service;
-
-public interface MqttService {
-    void connect();
-    void publish(String topic, String payload, int qos);
-    boolean isConnected();
-}
